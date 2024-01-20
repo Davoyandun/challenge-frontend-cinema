f609dd445f2c9e1165a1e7e44b2685232743489c
Davoyandun <yandundavid@gmail.com>
2024-01-20 12:12:06 -0500

    first commit

---
+++ javascript/constants.js
@@ -0,0 +1,11 @@
+export const API_URL =
+  "https://moviesdatabase.p.rapidapi.com/titles/random?list=most_pop_movies";
+export const API_MOVIE_ID_URL =
+  "https://moviesdatabase.p.rapidapi.com/titles/{id}?info=base_info";
+export const OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "82434afdeamshe6251846e5fe6acp1f20a5jsncdcae8b2ee15",
+    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  },
+};
